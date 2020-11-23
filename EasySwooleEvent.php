262e25555d10d944623e9a75bf98c3a39b88a768
wanghan <minglongoc@me.com>
2020-11-23 17:10:33 +0800

    2020-11-23_17:10:33

--- EasySwooleEvent.php
+++ EasySwooleEvent.php
@@ -14,12 +14,13 @@ class EasySwooleEvent implements Event
     public static function initialize()
     {
         date_default_timezone_set('Asia/Shanghai');
+
+        CreateMysqlOrm::getInstance()->create();
     }
 
     public static function mainServerCreate(EventRegister $register)
     {
         CreateRedisPool::getInstance()->create();
-        CreateMysqlOrm::getInstance()->create();
         CreateMysqlPool::getInstance()->create();
     }
 
