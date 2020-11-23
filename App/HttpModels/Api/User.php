781876fb1c2e712e4b9c72d66dd3b742ae229cb6
wanghan <minglongoc@me.com>
2020-11-23 18:04:05 +0800

    2020-11-23_18:04:05

--- App/HttpModels/Api/User.php
+++ App/HttpModels/Api/User.php
@@ -6,6 +6,11 @@ use App\HttpModels\ModelsBase;
 
 class User extends ModelsBase
 {
+    function __construct(array $data = [])
+    {
+        parent::__construct($data);
+    }
+
     protected $tableName = 'api_user';
     protected $autoTimeStamp = true;
     protected $createTime = 'created_at';
