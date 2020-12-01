4dd6e6ff6f844a047d279448cb60b1d151c83220
wanghan <minglongoc@me.com>
2020-12-01 10:55:13 +0800

    2020-12-01_10:55:13

--- App/HttpController/Api/UserController.php
+++ App/HttpController/Api/UserController.php
@@ -3,6 +3,7 @@
 namespace App\HttpController\Api;
 
 use App\HttpController\Index;
+use App\HttpModels\Admin\OneJoke;
 use App\HttpModels\Admin\OneSaid;
 use App\HttpModels\Api\LinkClick;
 use App\HttpService\Common\CreateMysqlTable;
@@ -49,13 +50,16 @@ class UserController extends Index
     {
         try
         {
+            $total = OneJoke::create()->count();
+
+            $res = OneJoke::create()->where('id',date('Ymd')%$total)->get();
 
         }catch (\Throwable $e)
         {
             return $this->writeErr($e,__FUNCTION__);
         }
 
-        return $this->writeJson(200,null,null);
+        return $this->writeJson(200,null,$res);
     }
 
 
