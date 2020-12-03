07f133a2a26dda119532f9f9d4522b32d1e91490
wanghan <minglongoc@me.com>
2020-12-03 17:13:28 +0800

    2020-12-03_17:13:28

--- App/HttpController/Api/UserController.php
+++ App/HttpController/Api/UserController.php
@@ -36,7 +36,7 @@ class UserController extends Index
         {
             $total = OneSaid::create()->count();
 
-            $res = OneSaid::create()->where('id',date('YmdH')%$total)->get();
+            $res = OneSaid::create()->where('id',date('YmdHi')%$total)->get();
 
         }catch (\Throwable $e)
         {
@@ -52,7 +52,7 @@ class UserController extends Index
         {
             $total = OneJoke::create()->count();
 
-            $res = OneJoke::create()->where('id',date('YmdH')%$total)->get();
+            $res = OneJoke::create()->where('id',date('YmdHi')%$total)->get();
 
         }catch (\Throwable $e)
         {
