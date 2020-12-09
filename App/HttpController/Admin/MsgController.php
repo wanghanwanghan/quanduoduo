0322c72b0fe517028d3d270b47dc53a4f5fbefa8
wanghan <minglongoc@me.com>
2020-12-09 17:16:31 +0800

    2020-12-09_17:16:31

--- App/HttpController/Admin/MsgController.php
+++ App/HttpController/Admin/MsgController.php
@@ -3,6 +3,8 @@
 namespace App\HttpController\Admin;
 
 use App\HttpController\Index;
+use App\HttpService\CoHttpClient;
+use App\HttpService\WxService;
 
 class MsgController extends Index
 {
@@ -13,24 +15,23 @@ class MsgController extends Index
 
     function push()
     {
+        $access_token = WxService::getInstance()->getAccessToken();
+        $access_token = $access_token['access_token'];
 
+        $url = "https://api.weixin.qq.com/cgi-bin/message/subscribe/send?access_token={$access_token}";
 
+        $data = [
+            'access_token' => $access_token,
+            'touser' => 'oDCC45HOXc-CC6XyqVfAjyM_N-zQ',
+            'template_id' => 'zyTie20yrJMNorCRpDv5v10NsBNnM0Qy2oZ6wvd4PU4',
+            'data' => jsonEncode([]),
+            'miniprogram_state' => 'trial',
+            'lang' => 'zh_CN',
+        ];
 
+        $res = (new CoHttpClient())->setDecode(true)->send($url,$data);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-        return $this->writeJson(200,null,null);
+        return $this->writeJson(200,null,$res);
     }
 
 
