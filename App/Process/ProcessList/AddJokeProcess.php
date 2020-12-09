a2568a6d24ec92bab8c4399b7cc6a89b626dcd05
wanghan <minglongoc@me.com>
2020-12-09 20:13:43 +0800

    2020-12-09_20:13:43

--- App/Process/ProcessList/AddJokeProcess.php
+++ App/Process/ProcessList/AddJokeProcess.php
@@ -2,8 +2,10 @@
 
 namespace App\Process\ProcessList;
 
+use App\HttpService\CoHttpClient;
 use App\HttpService\LogService;
 use App\Process\ProcessBase;
+use Carbon\Carbon;
 use Swoole\Process;
 
 class AddJokeProcess extends ProcessBase
@@ -21,6 +23,32 @@ class AddJokeProcess extends ProcessBase
     protected function addJoke()
     {
         //自定义进程不需要传参数，启动后就一直消费一个列队
+
+        $H = '';
+
+        while (true)
+        {
+            $nowH = (int)Carbon::now()->format('H');
+
+            if ($H !== $nowH)
+            {
+                $H = $nowH;
+
+                for ($page=1;$page<=4;$page++)
+                {
+                    $url = "http://v.juhe.cn/joke/content/text.php?page={$page}&pagesize=20&key=41b914ce994bc0e57d0fce86b0041a03";
+
+                    $res = (new CoHttpClient())->setDecode(true)->send($url,[],[],[],'get');
+
+                    LogService::getInstance()->log4PHP($res);
+                }
+
+            }else
+            {
+                \co::sleep(30);
+            }
+        }
+
     }
 
     protected function onPipeReadable(Process $process)
