737c5088fdb9fdee18bbb80d1a29fafe20559600
wanghan <minglongoc@me.com>
2020-12-09 17:27:31 +0800

    2020-12-09_17:27:31

--- App/HttpController/Admin/MsgController.php
+++ App/HttpController/Admin/MsgController.php
@@ -24,10 +24,10 @@ class MsgController extends Index
             'access_token' => $access_token,
             'touser' => 'oDCC45HOXc-CC6XyqVfAjyM_N-zQ',
             'template_id' => 'zyTie20yrJMNorCRpDv5v10NsBNnM0Qy2oZ6wvd4PU4',
-            'data' => jsonEncode([
+            'data' => json_encode([
                 'thing1' => ['value' => '胡大胖'],
                 'thing2' => ['value' => '胡小胖'],
-            ]),
+            ],JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES),
             'miniprogram_state' => 'trial',
             'lang' => 'zh_CN',
         ];
