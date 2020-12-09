37ec578fb3d8daaf9bb75a7bca7e09f7046d0008
wanghan <minglongoc@me.com>
2020-12-09 17:38:17 +0800

    2020-12-09_17:38:17

--- App/HttpController/Admin/MsgController.php
+++ App/HttpController/Admin/MsgController.php
@@ -24,10 +24,10 @@ class MsgController extends Index
             'access_token' => $access_token,
             'touser' => 'oDCC45HOXc-CC6XyqVfAjyM_N-zQ',
             'template_id' => 'zyTie20yrJMNorCRpDv5v10NsBNnM0Qy2oZ6wvd4PU4',
-            'page' => 'index',
+            'page' => 'coupon',
             'data' => [
                 'thing1' => ['value' => '胡大胖'],
-                'thing2' => ['value' => '胡小胖'],
+                'thing2' => ['value' => '快还钱'],
             ],
             'miniprogram_state' => 'developer',
             'lang' => 'zh_CN',
