6d6f7a52de5656a7c6c0916f8e11e02afc857ab3
wanghan <minglongoc@me.com>
2020-12-22 18:46:22 +0800

    2020-12-22_18:46:22

--- App/HttpController/Api/UserController.php
+++ App/HttpController/Api/UserController.php
@@ -22,7 +22,7 @@ class UserController extends Index
         $openId = $this->getRawData('openId');
 
         if (empty($linkId) || !is_numeric($linkId)) return $this->writeJson(201,null,null,'id错误');
-        if (empty($openId) || !is_numeric($openId)) return $this->writeJson(201,null,null,'openid错误');
+        if (empty($openId)) return $this->writeJson(201,null,null,'openid错误');
 
         try
         {
@@ -47,7 +47,7 @@ class UserController extends Index
         $openId = $this->getRawData('openId');
 
         if (empty($goodsId) || !is_numeric($goodsId)) return $this->writeJson(201,null,null,'id错误');
-        if (empty($openId) || !is_numeric($openId)) return $this->writeJson(201,null,null,'openid错误');
+        if (empty($openId)) return $this->writeJson(201,null,null,'openid错误');
 
         try
         {
